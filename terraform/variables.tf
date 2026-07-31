9bfed9168b8c557712284d0afe466a74838d401f
A-Marbach <marbach.a@gmx.net>
2026-07-31 22:44:02 +0200

    Add Terraform provider and variables

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region for all resources."
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "project_name" {
+  description = "Project name used for tagging."
+  type        = string
+  default     = "conduit-aws"
+}
+
+variable "environment" {
+  description = "Deployment environment."
+  type        = string
+  default     = "dev"
+}
