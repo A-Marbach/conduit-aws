0bcb5ab097e275ea6ad649766b09d4112497dfb6
A-Marbach <marbach.a@gmx.net>
2026-08-04 15:57:16 +0200

    Add AWS CloudWatch monitoring

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -18,6 +18,12 @@ resource "aws_iam_role" "ec2" {
   })
 }
 
+
+resource "aws_iam_role_policy_attachment" "cloudwatch" {
+  role       = aws_iam_role.ec2.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
 resource "aws_iam_role_policy_attachment" "ssm" {
   role       = aws_iam_role.ec2.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
